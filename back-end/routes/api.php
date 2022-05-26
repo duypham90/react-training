587d49c3686f8abd412dd0ac6532d5934c9f5584
Duy Pham <hongduyphp@gmail.com>
2022-05-26 18:13:08 +0700

    update router name

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -17,12 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
     Route::post('/login', [LoginController::class, 'login'])->name('login');
-});
-
-
-
-Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
-    Route::group(['prefix' => 'users'], function () {
+    Route::group(['prefix' => 'posts'], function () {
         Route::get('/',  [PostController::class, 'index'])->name('index');
         Route::get('/{id}',  [PostController::class, 'show'])->name('show');
     });
